ab4356e79ab1de8edafac3a2df7dbd0a17ec9811
Denyse Freitas Santos <denysefreitass@gmail.com>
2026-01-12 20:29:22 -0300

    feat(comandos): comandos iniciais em Python
    - organizar arquivos em pastas
    - implementar print e argumento sep
    - implementar operações aritméticas básicas
    - implementar variáveis do tipo string

---
+++ Códigos/02_calculadora.py
@@ -0,0 +1,16 @@
+# %%
+# SOMA
+print("1 + 1 =", 1 + 1)
+# SUBTRAÇÃO
+print("5 - 5 =", 5 - 5)
+# MULTIPLICAÇÃO
+print("5 x 3 =", 5 * 3)
+# DIVISÃO 
+print("100 / 4 =", 100 / 4)
+# DIVISÃO QUE EXIBE SOMENTE A PARTE INTEIRA
+print("100 // 3 =", 100 // 3)
+# RESTO DA DIVISÃO
+print("100 % 2 =", 100 % 2) 
+print("10 % 4 =", 10 % 4) 
+# POTENCIAÇÃO
+print("2 ** 3 =", 2 ** 3)
