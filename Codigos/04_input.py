d9199c68073faa6b66445deb45226b440a5c805c
Denyse Freitas Santos <denysefreitass@gmail.com>
2026-01-13 21:40:12 -0300

    feat(input): implementar testes com input em Python
    - ajustar organização dos arquivos e pastas
    - implementar uso do input
    - implementar exercícios básicos interativos
    - implementar conversão entre tipos

---
+++ Codigos/04_input.py
@@ -0,0 +1,6 @@
+print("Olá, mundo!")
+
+nome = input("Qual o seu nome? ")
+print("É uma honra te conhecer,", nome, "!")
+# input faz a leitura de uma string a partir da entrada padrão
+# se necessário, faça a conversão da string em outro tipo de dado (int, float, bool)
---
+++ Exercicios/003_historia.py
@@ -0,0 +1,15 @@
+print('O trecho abaixo foi extraído do site oficial do Rick Riordan, autor da série de livros "Percy Jackson e os Olimpianos". Trata-se do primeiro capítulo do primeiro volume da série.\n')
+input("Para iniciar e/ou continuar a história, aperte enter")
+
+p1 = "Olhe, eu não queria ser um meio-sangue. Se você está lendo isto porque acha que pode ser um, meu conselho é o seguinte: feche este livro agora mesmo. Acredite em qualquer mentira que sua mãe ou seu pai lhe contou sobre seu nascimento, e tente levar uma vida normal."
+
+p2 = "Ser um meio-sangue é perigoso. É assustador. Na maioria das vezes, acaba com a gente de um jeito penoso e detestável."
+
+p3 = "Se você é uma criança normal, que está lendo isto porque acha que é ficção, ótimo. Continue lendo. Eu o invejo por ser capaz de acreditar que nada disso aconteceu."
+
+# Garante que haja QUALQUER interação do usuário para continuar a história
+input(p1) 
+input(p2)
+input(p3)
+
+print("Fim do trecho! Acesse o capítulo completo gratuitamente no site do Rick Riordan!")
